14f047c8af0aedb9638bd4a8022cdd0f1a8f2303
John Sterling <john.sterling@gooengine.com>
2015-11-02 14:45:56 +0100

    Add seil and karabiner settings

---
+++ osx/karabiner.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
+
+$cli set private.f17_to_f10 1
+/bin/echo -n .
+$cli set private.f14_to_aumlaut 1
+/bin/echo -n .
+$cli set private.swedish_chars 1
+/bin/echo -n .
+$cli set private.f13_to_aring 1
+/bin/echo -n .
+$cli set private.f15_to_oumlaut 1
+/bin/echo -n .
+$cli set caps_lock_to_hyper 1
+/bin/echo -n .
+$cli set private.f18_to_f8 1
+/bin/echo -n .
+$cli set private.f16_to_f11 1
+/bin/echo -n .
+/bin/echo
