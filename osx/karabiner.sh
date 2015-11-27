dc9cb487f52c3c1317b24f6b0e81d912ef28668a
johnste <john@johnste.se>
2015-11-27 09:45:44 +0100

    seil and karabiner settings updated

--- osx/karabiner.sh
+++ osx/karabiner.sh
@@ -2,20 +2,34 @@
 
 cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
 
-$cli set private.f17_to_f10 1
+$cli set private.f16_to_f11 1
 /bin/echo -n .
-$cli set private.f14_to_aumlaut 1
+$cli set repeat.initial_wait 416
 /bin/echo -n .
 $cli set private.swedish_chars 1
 /bin/echo -n .
-$cli set private.f13_to_aring 1
+$cli set private.f14_to_aumlaut 1
+/bin/echo -n .
+$cli set f16_to_f8 1
 /bin/echo -n .
 $cli set private.f15_to_oumlaut 1
+/bin/echo -n .
+$cli set remap.swedish_commandL2commandL_english 1
+/bin/echo -n .
+$cli set f18_to_f11 1
+/bin/echo -n .
+$cli set f17_to_f10 1
+/bin/echo -n .
+$cli set private.f17_to_f10 1
+/bin/echo -n .
+$cli set remap.swedish_commandR2commandR_swedish 1
+/bin/echo -n .
+$cli set private.f13_to_aring 1
+/bin/echo -n .
+$cli set repeat.wait 33
 /bin/echo -n .
 $cli set caps_lock_to_hyper 1
 /bin/echo -n .
 $cli set private.f18_to_f8 1
-/bin/echo -n .
-$cli set private.f16_to_f11 1
 /bin/echo -n .
 /bin/echo
